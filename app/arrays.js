9d668dcecc65bccfc0f6c7254cd81ae2213689f1
Gabriel Lima <contato@gabrielmateus.com>
2016-04-28 19:36:36 -0300

    Answer array's remove

--- app/arrays.js
+++ app/arrays.js
@@ -16,7 +16,13 @@ exports.arraysAnswers = {
     },
 
     remove: function(arr, item) {
+        for(var i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] === item) {
+               arr.splice(i, 1);
+            }
+        }
 
+        return arr;
     },
 
     removeWithoutCopy: function(arr, item) {
