2e54cd537ea3558070a68df15a30c3c1a5cd4c42
Gabriel Lima <contato@gabrielmateus.com>
2016-05-03 19:05:25 -0300

    Answer array's prepend, curtail, concat and insert

--- app/arrays.js
+++ app/arrays.js
@@ -50,19 +50,39 @@ exports.arraysAnswers = {
     },
 
     prepend: function(arr, item) {
+        arr.unshift(item);
 
+        return arr;
     },
 
     curtail: function(arr) {
+        arr.shift();
 
+        return arr;
     },
 
     concat: function(arr1, arr2) {
+        var newArr = arr1.concat(arr2);
 
+        return newArr;
     },
 
     insert: function(arr, item, index) {
+        var arr1 = [];
+        var arr2 = [];
 
+        for(var i = 0; i < arr.length; i++) {
+            if(arr[i] < arr[index]) {
+                arr1.push(arr[i]);
+            } else {
+                arr2.push(arr[i]);
+            }
+        }
+
+        arr1.push(item);
+        arr = arr1.concat(arr2);
+
+        return arr;
     },
 
     count: function(arr, item) {
