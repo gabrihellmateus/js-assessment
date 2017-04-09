b0cf7c67745948b79ca19c3cdc057132d2692ea6
Gabriel Lima <naive505@gmail.com>
2017-04-09 23:41:48 +0100

    Add count, duplicates and more of array tests

--- app/arrays.js
+++ app/arrays.js
@@ -86,18 +86,66 @@ exports.arraysAnswers = {
     },
 
     count: function(arr, item) {
+      var arrMatches = [];
+      var counter = 0;
 
+      for (var i = 0; i < arr.length; i++) {
+         if (arr[i] === item) {
+           arrMatches.push(arr[i]);
+         }
+      }
+
+      if (arrMatches.length) {
+        for (var j = 0; j < arrMatches.length; j++) {
+           counter++;
+        }
+      }
+
+      return counter;
     },
 
     duplicates: function(arr) {
+      var arrPlain = [];
+      var arrDuplicates = [];
 
+    	for (var i = 0; i < arr.length; i++) {
+    		if (arrPlain.indexOf(arr[i]) === -1) {
+          arrPlain.push(arr[i]);
+    		} else if (arrDuplicates.indexOf(arr[i]) === -1) {
+          arrDuplicates.push(arr[i]);
+        }
+    	}
+
+  		return arrDuplicates;
     },
 
     square: function(arr) {
+      var arrSquare = [];
+      var result;
 
+  		for (var i = 0; i < arr.length; i++) {
+        result = arr[i] * arr[i];
+
+        arrSquare.push(result);
+  		}
+
+      return arrSquare;
     },
 
     findAllOccurrences: function(arr, target) {
+      var first = arr.indexOf(target);
+    	var arrOcc = [];
 
+    	if (first !== -1) {
+    		for (var i = first; i < arr.length ; i++) {
+    			var match = arr.indexOf(target, i);
+
+    			if (match !== -1 && arrOcc.indexOf(match) === -1) {
+    				arrOcc.push(match);
+    			}
+    		}
+    	}
+
+      return arrOcc;
     }
 };
